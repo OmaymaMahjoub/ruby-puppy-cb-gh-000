9896f122338599fc866043bba59acf127c41bbf9
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 14:01:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,30 @@
 # Add your code here
 class Dog
   @@all=[]
+
   attr_reader :name
+
   def initialize(name)
     @name=name
     @@all<<self
   end
+
   def self.print_all
     @@all.each do |d|
       puts d.name
     end
   end
+
   def self.clear_all
     @@all=[]
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.save
+    add<<self
+  end
+  
 end
