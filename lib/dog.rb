375c0ff4a44156f6126ae515616c7d82994ce94f
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 13:59:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Add your code here
+class Dog
+  @@all=[]
+  attr_reader :name
+  def initialize(name)
+    @name=name
+    @@all<<self     
+  end
+  def self.print_all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+end
